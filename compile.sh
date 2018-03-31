a2757e84995f4b4beefd954bd4db6a455649f863
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-03-31 08:28:01 -0400

    Main changes

--- compile.sh
+++ compile.sh
@@ -10,31 +10,28 @@ set -e
 menu()
 {
 	echo
-	echo "  ****************************************************"
-	echo "  * Welcome to the C++ compiler & executable program *"
-	echo "  ****************************************************"
+	echo "  *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+	echo "  * Welcome to the C++ compiler & executable program  *"
+	echo "  *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
 	echo
 }
 
 # Lists the files in the current directory
 directory()
 {
-	echo "________________________________________________________"
-	echo " Files in $PWD :"
+	echo "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*"
 	echo
-
-	for files in *;
-	do
-		echo $files
-	done 
-	echo "________________________________________________________"
+	ls -t
+	echo 
+	echo "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*"
 }
 
 compile()
 {
+	menu
 	directory
 	echo
-	echo "Please enter the name of the file you'd like to compile:"
+	echo "* Please enter the name of the file you'd like to compile: *"
 
 	read name # Reads input from keyboard.
 	fileName=${name%.*}.out #Reads file name from left to right up until the '.' 
@@ -48,28 +45,50 @@ compile()
 
 	# Conditional statement - Does the user want to run / execute program.
 	if [ "$answer" = "y" ] || [ "$answer" = "Y" ]; then
-	echo "_______________________________________________________"
-	echo "         ***********************************"
+	echo "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*"
+	echo "         *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
 	echo "         * Here is the output of $fileName *"
-	echo "         ************************.o***********"
+	echo "         *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+	echo
 	./$fileName
 	echo
-	echo "_______________________________________________________"
+	echo "*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*_*"
 	else
 		leave
-		exit
 	fi
+
 }
+
 
 clean()
 {
-	echo ".out file has been deleted."
+
+	echo
+	echo "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+	echo "*  Removing files with the extension .out   *"
+	echo "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+	echo
+	echo -ne '#####                     (33%)\r'
+	sleep 1
+	echo -ne '#############             (66%)\r'
+	sleep 1
+	echo -ne '#######################   (100%)\r'
+	echo -ne '\n'
 	rm -f *.out
+	echo
+	echo "*-*-*-*-*-*-*-*-*"
+	echo "* File removed  *"
+	echo "*-*-*-*-*-*-*-*-*"
+
+	exit
 }
 
 leave()
 {
-	echo "Goodbye!, Until Next Time :)"
+	echo
+	echo "*-*-*-*-*-*-*-*-*-*-*"
+	echo "*  Exiting compiler *"
+	echo "*-*-*-*-*-*-*-*-*-*-*"
 	exit
 }
 if [ "$1" == "--clean" ]; then
@@ -77,9 +96,10 @@ if [ "$1" == "--clean" ]; then
 	leave
 fi
 
+
 if [ -n "$1" ]; then
 	echo "Invalid argument."
 	exit 1
 fi
-menu
+
 compile
