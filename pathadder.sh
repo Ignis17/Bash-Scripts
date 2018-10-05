b66796fb31fc766ca9ae22b30c6d19505adb6758
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-10-05 03:40:09 -0400

    - Added documentation to pathadder.sh

--- pathadder.sh
+++ pathadder.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Author: Joel Turbi
+# Created: 10/1/2018
+# This bash script creates a bin folder/directory under user's home directory & appends such location to PATH enviromental variable.
+
 # Creates bin folder under user's directory
 mkdir $HOME/bin
 # Add bin folder to Enviromental PATH variable
