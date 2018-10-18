5c56d8b482bcfebe24d6a4f7dcf13d7d93c8e760
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-10-18 15:39:03 -0400

    - Changed name of previous file named gitup.sh to push.sh

--- push.sh
+++ push.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Author: Joel Turbi
+# Created: 10/17/2018
+# Description: This script automates the stages of git:
+# 	Untracked: the file exists, but is not part of git's version control.
+#	Staged: the file has been added to git's version control but changes have not been committed.
+# 	Committed: the change has been committed
 
 # Function to automate github staging process:
 
@@ -8,10 +14,12 @@ push(){
 	git add .
 	git commit -a -m "$msg"
 	git push -u origin master
+	echo
 	echo "****************************************"
 	echo "* Changes have been uploaded to GitHub *"
 	echo "****************************************"
 }
+
 menu(){
 	echo
 	echo "*********************************************************************"
