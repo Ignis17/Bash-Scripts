bb27be03c89cb0455ed5dc26f95989822ab21fc0
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-10-20 23:17:49 -0400

    last minute changes.

--- push.sh
+++ push.sh
@@ -6,13 +6,10 @@
 #	Staged: the file has been added to git's version control but changes have not been committed.
 # 	Committed: the change has been committed
 
-# Set Bash to quit script and exit on errors:
-set -e
 
 # Function to automate github staging process:
 # Stages all files by adding them and committing them to Github
 push(){
-	push_s
 	echo
 	echo "E.g. to add everything to the staging area, enter "\"."\" or name of the file."
 	echo
@@ -22,98 +19,98 @@ push(){
 	git commit -a -m "$msg"
 	git push -u origin master
 	echo
-	echo "*======================================*"
 	echo "| Changes have been uploaded to GitHub |"
-	echo "*======================================*"
 }
 
 # Displays this message when script is run for the first time.
 menu(){
-	echo
-	echo "*====================================================================*"
-	echo "| This script automates the process of submitting changes to github  |"
-	echo "*====================================================================*"
-	echo
-	echo "1) Check for recent changes made in local repository."
-	echo "2) Start staging and commit process."
-	echo "3) Pull recent changes from remote repository."
-	echo "4) Push commited changes to remote repository."
-	echo "5) Github configuration set up."
-	echo "6) Clone a repository by url."
-	echo "7) Exit."
-	read -p "Enter your choice: " reply
-	case $reply in
-		"1")
-			push_s;
-			exit 0;;
-		"2")
-			push;
-			exit 0;;
-		"3")
+	while [[ $reply != 7 ]]; do
+		clear
+		echo "*====================================================================*"
+		echo "| This script automates the process of submitting changes to github  |"
+		echo "*====================================================================*"
+		echo
+		echo "
+		| Enter selection |
+
+		1) Check for recent changes in local repository.
+		2) Stage & commit changes from local repository.
+		3) Pull recent changes from remote repository.
+		4) Push commited changes to remote repository.
+		5) Github configuration setup.
+		6) Clone a repository by url.
+		7) Exit.
+		"
+		read -p "Selection #: " reply
+		if [ $reply == 1 ]; then
+			push_s
+			sleep 2
+		elif [ $reply == 2 ]; then
+			push
+			sleep 2
+
+		elif [ $reply == 3 ]; then
 			pull;
-			exit 0;;
-		"4")
-			git push;
-			exit 0;;
-		"5")
-			config;
-			exit 0;;
-		"6")
-			echo;
-			read -p "Paste url here: " url;
-			git clone "$url";
-		       	exit 0;;
-	
-		"7")
-			echo;
-			echo "Goodbye!";
-			exit 0 ;;
-		*) echo "Invalid choice"; exit 1 ;;
-	esac
+			sleep 2
+
+		elif [ $reply == 4 ]; then
+			git push
+			sleep 2
+
+		elif [ $reply == 5 ]; then
+			config
+			sleep 2
+
+		elif [ $reply == 6 ]; then
+			echo
+			read -p "Paste url here: " url
+			git clone "$url"
+		  sleep 2
+		elif [ $reply == 7 ]; then
+			echo
+			echo "Program terminated. Goodbye! :)"
+			exit 0
+		else
+			echo "Invalid selection."
+			echo "Try again."
+			sleep 1
+		fi
+	done
 }
 
 # Checks for changes made to current repository:
 push_s(){
-	echo "*============================*"
+	echo
 	echo "| Checking for local changes |"
-	echo "*============================*"
 	echo
 	git status
 	echo
-	echo "*===========================*"
 	echo "| Done checking for changes |"
-	echo "*===========================*"
-}	
+}
 
 pull(){
-	echo "*=============================*"
 	echo "| Checking for recent changes |"
-	echo "*=============================*"
 	echo
 	git pull
-	echo 
-	echo "*==================================*"
+	echo
 	echo "| Done checking for recent changes |"
-	echo "*==================================*"
 }
 
 # Configures Github Enviroment to user's info.
 
 config(){
 	echo
-	echo "*=======================================*"	
+	echo "*=======================================*"
 	echo "| Welcome to Github Configuration Setup |"
 	echo "*=======================================*"
 	echo
 	read -p "Enter Username: " user
-	echo 
+	echo
 	read -p "Enter Email address: " email
 	echo
 	git config --global user.name "$user"
 	git config --global user.email "$email"
-	echo "*========================*"
 	echo "| Changes have been made |"
-	echo "*========================*"
 }
 
 # Function calls:
