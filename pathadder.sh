0d05e0e4d9f11efbd464e85d7b29ca9640fb9a0e
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-10-26 19:02:33 -0400

    Fixed bug issue

--- pathadder.sh
+++ pathadder.sh
@@ -7,4 +7,4 @@
 # Creates bin folder under user's directory
 mkdir $HOME/bin
 # Add bin folder to Enviromental PATH variable
-echo "export PATH='\$PATH:~/bin'" >> $HOME/.bashrc && echo "Done!"
+echo "export PATH=\"\$PATH:~/bin\"" >> $HOME/.bashrc && echo "Done!"
