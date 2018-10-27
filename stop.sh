e3510ac10a0c5f2a274ace6453a1623fa9bcbdaf
ignis17 <joel.turbi@student.mec.cuny.edu>
2018-10-27 02:58:50 -0400

    Modified stop.sh script

--- stop.sh
+++ stop.sh
@@ -8,10 +8,38 @@
 menu(){
 	echo "	Make a selection: "
 	echo
-	echo "- 1) Kill/stop predetermined processes(E.g. Apache2,Cassandra, Mongodb, MySQL)."
-	echo "- 2) Enter name of process you'd like to kill/stop."
-	echo "- 3) Exit."
+	echo "	- 1) Kill/stop predetermined processes(E.g. Apache2,Cassandra, Mongodb, MySQL)."
+	echo "	- 2) Stop predetermined processes(E.g. Apache2, Cassandra, Mongodb, MySQL), from auto-starting."
+	echo "	- 3) Re-enable predetermined processes to auto-start at boot-up."
+	echo "	- 4) Enter name of process you'd like to kill/stop."
+	echo "	- 5) Exit."
 	echo
+}
+
+auto-enable(){
+	echo "Starting..."
+	sudo systemctl enable cassandra
+	sudo systemctl start cassandra
+	sudo systemctl enable apache2
+	sudo systemctl start apache2
+	sudo systemctl enable mongodb
+	sudo systemctl start mongodb
+	sudo systemctl enable mysql
+	sudo systemctl start mysql
+	echo "Done."
+}
+
+auto-disable(){
+	echo "Starting..."
+	sudo systemctl stop cassandra
+	sudo systemctl disable cassandra
+	sudo systemctl stop apache2
+	sudo systemctl disable apache2
+	sudo systemctl stop mongodb
+	sudo systemctl disable mongodb
+	sudo systemctl stop mysql
+	sudo systemctl disable mysql
+	echo "Done."
 }
 
 # function to perform user's selection
@@ -24,11 +52,13 @@ stop(){
 		/etc/init.d/cassandra stop;
 		/etc/init.d/mongodb stop;
 		/etc/init.d/mysql stop;;
-		2) read -p "Enter name of process: " choice; /etc/init.d/"$choice" stop;;
-		3) echo "Done"; exit 0;;
+		2) auto-disable;;
+		3) auto-enable ;;
+		4) read -p "Enter name of process: " choice; /etc/init.d/"$choice" stop;;
+		5) echo "Done"; exit 0;;
 		*) echo "Invalid selection. Try again!"; exit 1;;
 	esac
 }
 
-# Function calls: 
+# Function calls:
 stop
